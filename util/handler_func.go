53309129b117c4d16993a379aa1b8c428e46f99e
best2000 <silentman@hotmail.co.th>
2024-01-26 02:29:01 +0700

    err handling

--- util/handler_func.go
+++ util/handler_func.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"errors"
 	"log/slog"
 	"net/http"
 )
@@ -9,11 +10,29 @@ import (
 type HandlerFunc func(w ResponseWriter, r *http.Request) error
 
 func (f HandlerFunc) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
 	//call main handler
 	err := f(ResponseWriter{ResponseWriter: w}, r)
+
 	//error handling...
 	if err != nil {
 		slog.Error(err.Error())
-		w.Write([]byte(err.Error()))
+
+		//prepare error response
+		//extract ApiError from error
+		var apiErr ApiError
+		if errors.As(err, &apiErr) {
+			//ApiError type
+			w.WriteHeader(apiErr.HttpStatus)
+			w.Write([]byte(apiErr.Error()))
+		} else {
+			//std error
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+		}
+		return
+	} else {
+		//normal case, no error
+		w.WriteHeader(http.StatusOK)
 	}
 }
